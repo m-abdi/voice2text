413b6900b307998c3cd4311dceba11b35fbb75fe
Mehdi Abdi <m.abdi.public@gmail.com>
2024-02-14 23:49:19 +0330

    New package version

--- dist/text/caption.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Caption {
-    protected element: any;
-    track: any;
-    protected lastCue: TextTrackCue;
-    constructor(element: Element);
-    addCaption(label: string, lang: string, text?: string, endTime?: number): void;
-    addText(startTime: number, endTime: number, captionText: string): void;
-    clearCaption(): void;
-}
