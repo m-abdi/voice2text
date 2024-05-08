92e950dbabb00984d0f9e47955e1aefae2c4de08
Mehdi Abdi <m.abdi.public@gmail.com>
2024-05-08 18:14:28 +0330

    Add new build

--- dist/voice2text.js
+++ dist/voice2text.js
@@ -304,7 +304,7 @@ class VoiceToText {
             this.source = document.querySelector(options.source);
         }
         else {
-            this.source = this.source;
+            this.source = options.source;
         }
         if (!options?.converter || options?.converter === "vosk") {
             this.converter = new Vosk({
